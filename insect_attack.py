f7be8234b9d07ed75cd1eaf1a1485b4869cd5535
Vyacheslav <vyacheslav@MacBook-Air-Vyacheslav.local>
2022-02-12 13:48:38 +0700

    refactoring the run_game method with helper methods _check_event() and _update_screen()

--- insect_attack.py
+++ insect_attack.py
@@ -17,15 +17,23 @@ class InsectAttack:
     def run_game(self):
         '''Start the main game loop'''
         while True:
-            # Monitor keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # The screen is redrawn on each iteretion of the loop
-            self.screen.fill(self.settings.bg_color)
-            self.frog.blitme()
-            # Displaying the last drawn screen
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+      '''Handles key presses and mouse event'''
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              sys.exit()
+
+    def _update_screen(self):
+        '''Refreshes the screen image and displays the new screen'''
+        # The screen is redrawn on each iteretion of the loop
+        self.screen.fill(self.settings.bg_color)
+        self.frog.blitme()
+        # Displaying the last drawn screen
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # creating an instance and launching the game
