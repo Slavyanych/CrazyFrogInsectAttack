bcd97c4018c00b5183ce95d36eb852777f834587
Vyacheslav <vyacheslav@MacBook-Air-Vyacheslav.local>
2022-02-12 12:31:15 +0700

    All game settings are placed in a separete class.Created and added a frog to the screen

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    '''Class to store all game settings'''
+    def __init__(self):
+        '''Initializes game settigns'''
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (166, 175, 255)
+
